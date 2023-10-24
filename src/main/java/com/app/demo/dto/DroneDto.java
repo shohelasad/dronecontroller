cf77e66e7cd0fc45e17586ed95a38bb2277b9770
shohelasad <shohel.asad@gmail.com>
2023-10-25 01:38:03 +0200

    refactor code

--- src/main/java/com/app/demo/dto/DroneDto.java
+++ src/main/java/com/app/demo/dto/DroneDto.java
@@ -3,5 +3,5 @@ package com.app.demo.dto;
 import com.app.demo.enums.Direction;
 import jakarta.validation.constraints.Min;
 
-public record DroneDto(Long id, @Min(value = 0, message = "Value must be greater than or equal to 0") int x, @Min(value = 0, message = "Value must be greater than or equal to 0") int y, Direction direction) {
+public record DroneDto(Long id, @Min(value = 0, message = "Value must be greater than or equal to 0") Integer x, @Min(value = 0, message = "Value must be greater than or equal to 0") Integer y, Direction direction) {
 }
